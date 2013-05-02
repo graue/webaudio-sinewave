e5476266d2ee0305de78a228abf6836b6f651007
Scott Feeney <scott@oceanbase.org>
2013-05-02 14:20:20 -0400

    Sine now being generated, but glitchy
    When you load the page for the first time it works.
    But after reloading or using Back/Forward, it only calls the processing
    function twice and then reuses that buffer over and over
    for some reason. (Chromium 25, Linux)

--- sinewave.js
+++ sinewave.js
@@ -8,5 +8,27 @@
         alert('So sad! You do not have Web Audio support.');
         return;
     }
-    // DO STUFF with ctx
+
+    var node = ctx.createJavaScriptNode(16384, 1, 1);
+    var rate = ctx.sampleRate; // samples/sec
+    var sineFreq = 1000; // cycles/sec
+    var sineLength = 5; // play for 5 seconds
+    window.n = 0; // current sample number
+
+    node.onaudioprocess = function(e) {
+        //console.log('onaudioprocess called');
+        var data = e.outputBuffer.getChannelData(0);
+        //console.log('at start, n is ' + window.n);
+        for (var i = 0; i < data.length; i++) {
+            if (window.n < rate*sineLength)
+                data[i] = Math.sin(window.n*2*Math.PI*sineFreq/rate);
+            else
+                data[i] = 0;
+
+            window.n++;
+        }
+        //console.log('at end, n is ' + window.n);
+    };
+
+    node.connect(ctx.destination);
 });
