c06665d5915d5b05a5b34853d56b92938008bf53
Scott Feeney <scott@oceanbase.org>
2013-05-12 21:47:31 -0400

    Add Start button to initialize audio
    This fixes the bugs I've been experiencing, though buffer size
    still seems to need to be 2048+, or there's no sound.
    (Chromium 25, Linux)

--- sinewave.js
+++ sinewave.js
@@ -1,4 +1,13 @@
-;$(document).ready(function() {
+$(document).ready(function() {
+    $('#btn-start').click(startAudio);
+});
+
+var alreadyStarted = false;
+
+function startAudio() {
+    if (alreadyStarted)
+        return;
+    alreadyStarted = true;
     var ctx;
     if (typeof AudioContext !== 'undefined') {
         ctx = new AudioContext();
@@ -9,16 +18,14 @@
         return;
     }
 
-    var node = ctx.createJavaScriptNode(16384, 1, 1);
+    var node = ctx.createScriptProcessor(2048, 1, 1);
     var rate = ctx.sampleRate; // samples/sec
     var sineFreq = 1000; // cycles/sec
     var sineLength = 5; // play for 5 seconds
     window.n = 0; // current sample number
 
     node.onaudioprocess = function(e) {
-        //console.log('onaudioprocess called');
         var data = e.outputBuffer.getChannelData(0);
-        //console.log('at start, n is ' + window.n);
         for (var i = 0; i < data.length; i++) {
             if (window.n < rate*sineLength)
                 data[i] = Math.sin(window.n*2*Math.PI*sineFreq/rate);
@@ -27,8 +34,7 @@
 
             window.n++;
         }
-        //console.log('at end, n is ' + window.n);
     };
 
     node.connect(ctx.destination);
-});
+}
